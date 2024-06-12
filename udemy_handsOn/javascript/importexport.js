afc93e3a904816d524165945133dd384711bafc7
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-12 12:03:21 +0530

    impots and exports variables and functions

---
+++ udemy_handsOn/javascript/importexport.js
@@ -0,0 +1,17 @@
+//Normal Import 
+import minus,{pi,add} from './utils.js'
+console.log(pi)
+let a=5,b=4
+console.log(`Addition${a} and ${b}  is ${add(a,b)}`)
+console.log(`Substration ${a} and ${b} is ${minus(a,b)}`)
+
+
+
+//import all exported Modules 
+// import * as utils from'./utils.js'
+// console.log(utils.name)
+// console.log(utils.getName())
+
+
+
+
---
+++ udemy_handsOn/javascript/utils.js
@@ -0,0 +1,20 @@
+
+// Normal Export
+const pi=3.14
+
+ function add(a,b)
+{
+    return a+b;
+} 
+export {pi,add}
+
+// multiple export 
+
+// const name="Sumit"
+// function getName(){return "Salesforce"}
+// export {name,getName} 
+
+export default function minus(a,b){
+return a-b
+} 
+
