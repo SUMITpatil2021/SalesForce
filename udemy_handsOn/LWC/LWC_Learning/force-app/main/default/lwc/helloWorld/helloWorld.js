3035d4144455b542e6c1de7d36fca0e8965ecb1b
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-20 12:47:43 +0530

    Two Way Data Binding

--- udemy_handsOn/LWC/LWC_Learning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ udemy_handsOn/LWC/LWC_Learning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    Fullname="Salesforce Developer "
+   
+    name="Sumit"
+    title="Developer"
+
+    chnageHandler(event){
+        this.title=event.target.value
+    }
+}
