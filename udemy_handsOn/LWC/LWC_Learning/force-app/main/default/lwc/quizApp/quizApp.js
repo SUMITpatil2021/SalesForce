1cd7fed78635b3489501ff5ed11a03dcad81b07d
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-25 20:51:23 +0530

    setting  quiz app

---
+++ udemy_handsOn/LWC/LWC_Learning/force-app/main/default/lwc/quizApp/quizApp.js
@@ -0,0 +1,38 @@
+import { LightningElement } from 'lwc';
+
+export default class QuizApp extends LightningElement {
+    myQuestions=[
+        {
+            id:"Question1",
+            question:"Which one of the following is not a template loop ?",
+            answers:{
+                a:"for:each",
+                b:"iterator",
+                c:"map loop"
+            },
+            correctAnswer:"c"
+        },
+        {
+            id:"Question2",
+            question:"Which one of the following is invalid file in LWC ? ",
+            answers:{
+                a:".svg",
+                b:".apex",
+                c:".js"
+            },
+            correctAnswer:"b"
+        },
+
+        {
+            id:"Question3",
+            question:"Which one of the following is not a directive ?",
+            answers:{
+                a:"for:each",
+                b:"if:true",
+                c:"@track"
+            },
+            correctAnswer:"c"
+        }
+
+    ]
+}
