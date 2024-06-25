5ba8a66ba56d49d26da914c1a389db73a0bf4230
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-26 00:14:00 +0530

    Quiz App Using LWC completed

--- udemy_handsOn/LWC/LWC_Learning/force-app/main/default/lwc/quizApp/quizApp.js
+++ udemy_handsOn/LWC/LWC_Learning/force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,6 +1,10 @@
 import { LightningElement } from 'lwc';
 
 export default class QuizApp extends LightningElement {
+    selected={}  //for storing answers
+    correctAnswers=0 //to show number of correct answers
+    isSubmitted=0   // flag used to show the result 
+    // array of questions ans answers 
     myQuestions=[
         {
             id:"Question1",
@@ -8,7 +12,7 @@ export default class QuizApp extends LightningElement {
             answers:{
                 a:"for:each",
                 b:"iterator",
-                c:"map loop"
+                c:"map loop" 
             },
             correctAnswer:"c"
         },
@@ -35,4 +39,35 @@ export default class QuizApp extends LightningElement {
         }
 
     ]
+    
+//used for the disabling the submit button 
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length===this.myQuestions.length)
+    }
+//for  applying Dyanamic styling to our result  
+    get isScoredFull(){
+        return `slds-text-heading_large ${this.myQuestions.length===this.correctAnswers?
+            'slds-text-color_success':'slds-text-color_error'}`
+    }
+
+    changeHandler(event){
+        console.log("name: " ,event.target.name)
+        console.log("value : ", event.target.value)
+        const {name,value}=event.target
+        this.selected={...this.selected,[name]:value}
+
+    }
+
+    submitHandler(event){
+        event.preventDefault()
+        let correct=this.myQuestions.filter(item=>this.selected[item.id]===item.correctAnswer)
+        this.correctAnswers=correct.length
+        console.log("   this.correctAnswers",this.correctAnswers)
+        this.isSubmitted=true
+    }
+
+    resetHandler(event){
+        this.selected={}
+        this.correctAnswers=0
+    }
 }
