94abcf21dae05393d20eeef1582b157fe801f5ef
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-25 20:19:14 +0530

    iterator loop

--- udemy_handsOn/LWC/LWC_Learning/force-app/main/default/lwc/looping/looping.js
+++ udemy_handsOn/LWC/LWC_Learning/force-app/main/default/lwc/looping/looping.js
@@ -1,5 +1,29 @@
+import Company from '@salesforce/schema/Lead.Company';
 import { LightningElement } from 'lwc';
 
 export default class Looping extends LightningElement {
     carList=["Maruti","Tata", "KIA","Rolls-Royce", "Porche","Tesla"]
+
+    ceoList=[
+        {
+            id:1,
+            company:"Google",
+            name:"Sundar Pichai"
+        },
+        {
+            id:2,
+            company:"Amezon",
+            name:"Joes Bezos"
+        },
+        {
+            id:3,
+            company:"Tesla",
+            name:"Elon Musk"
+        },
+        {
+            id:4,
+            company:"Microsoft",
+            name:"Team Cook"
+        }
+    ]
 }
